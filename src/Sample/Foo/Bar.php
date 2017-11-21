4960b1e7853248c060e56aebb63155ed12fb66a5
zhangjintao <hi.zhangjt@gmail.com>
2017-11-21 21:26:09 +0800

    add src

---
+++ src/Sample/Foo/Bar.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Sample\Bar
+
+class Bar
+{
+    public static function info()
+    {
+        echo "bar\n";
+    }
+}
---
+++ src/Sample/Foo/Foo.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Sample\Foo;
+
+class Foo
+{
+    public static function info()
+    {
+        echo "foo\n";
+    }
+}
